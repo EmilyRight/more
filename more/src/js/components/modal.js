13b1884cdd117b0453c32d3ca7a717abc80719fc
PHOENIXIT\ekanarskaya <ekanarskaya@fil-it.ru>
2023-12-27 02:57:55 +0300

    fixes

--- more/src/js/components/modal.js
+++ more/src/js/components/modal.js
@@ -8,14 +8,21 @@ function defineModal(modalBoxName) {
   const modalBoxesList = document.querySelectorAll('.modal-box');
   const [modalBox] = Array.from(modalBoxesList)
     .filter((modal) => modal.dataset.modal === modalBoxName);
-  console.log(modalBox, modalBoxName);
   return modalBox;
+}
+function getScrollbarWidth() {
+  const documentWidth = document.documentElement.clientWidth;
+  const windowsWidth = window.innerWidth;
+  const scrollbarWidth = windowsWidth - documentWidth;
+  return scrollbarWidth;
 }
 
 export function openModal(modalBoxName) {
   const modalBox = defineModal(modalBoxName);
 
   const modal = modalBox.closest('.popup-modal');
+
+  body.style.paddingRight = `${getScrollbarWidth()}px`;
   body.classList.add(noscroll);
   modal.classList.remove(hidden);
   modalBox.classList.add(active);
@@ -36,5 +43,6 @@ export function openModal(modalBoxName) {
 
 export function closeModal(modalBox) {
   body.classList.remove(noscroll);
+  body.style.paddingRight = '0px';
   modalBox.closest('.popup-modal').classList.add(hidden);
 }
